7c111244632864ec158dcf2af3e40764b24fa28c
Tiago Freitas <70216659+FreitsTiago@users.noreply.github.com>
2021-01-09 16:28:43 -0300

    Tirando o node Modules

--- server.js
+++ server.js
@@ -6,25 +6,26 @@ const server_name = 'Fuego Chat';
 
 var __dirname = 'public';
 
-app.get('/', function (req, res) {
-  var content = fs.readFileSync(__dirname + "/index.html");
-  res.end(content);
-});
+app.use('/', function (req, res) {
+    if (req.url == '/') {
+        var content = fs.readFileSync(__dirname + '/index.html');
+        res.end(content);
+    } else {
+        try {
+            try {
+                var content = fs.readFileSync(__dirname + req.url + '.html');
+                res.end(content);
+            } catch {
+                var content = fs.readFileSync(__dirname + req.url);
+                res.end(content);
+            };
+        } catch {
+            var content = fs.readFileSync(__dirname + '/404.html');
+            res.status(404);
+            res.end(content);
+        };
+    };
 
-app.use('/files', function (req, res) {
-  try {
-    try {
-      var content = fs.readFileSync(__dirname + req.url + '.html');
-      res.end(content);
-    } catch {
-      var content = fs.readFileSync(__dirname + req.url);
-      res.end(content);
-    }
-  } catch {
-    var content = fs.readFileSync(__dirname + '/404.html');
-    res.status(404);
-    res.end(content);
-  }
 });
 
 function get_hour() {
@@ -93,5 +94,5 @@ io.on("connection", function (client) {
 
 
 http.listen(19132, function () {
-  console.log('listening on port 8080');
+  console.log('Server ONLINE');
 });
